390a1af2cc5beca1397351d106e25dc420dfec6a
Ruslan-dev-1996 <Kasabolotov96.emil.ru>
2019-10-18 19:05:14 +0600

    first commit

---
+++ source/webapp/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+from webapp.models import Article, Comment
+
+
+class ArticleForm(forms.ModelForm):
+    tags = forms.CharField(max_length=200, required=False, label='Тег')
+    class Meta:
+        model = Article
+        exclude = ['created_at', 'updated_at', 'tags']
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        exclude = ['created_at', 'updated_at']
+
+
+class ArticleCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['author', 'text']
+
+
+class SimpleSearchForm(forms.Form):
+    search = forms.CharField(max_length=100, required=False, label='Найти')
